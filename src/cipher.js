c69bc957cb184e78c82dd6de519daf58278b638c
natis93 <natis93_2011@hotmail.com>
2023-05-17 16:28:29 -0500

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,54 @@
+
 const cipher = {
-  // ...
+  encode: function (desplazamiento, texto) {
+    if (!texto.length || !desplazamiento) {
+      throw new TypeError()
+    }
+    let resultado = "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    desplazamiento = (desplazamiento % 26 + 26) % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+
+        if (letras.indexOf(texto[i]) !== -1) {
+          const posicion = ((letras.indexOf(texto[i]) + desplazamiento) % 26);
+          resultado += letras[posicion];
+
+        }
+        else
+          resultado += texto[i];
+      }
+    }
+
+    return resultado;
+  },
+
+  decode: function (desplazamiento, texto) {
+    if (!texto.length || !desplazamiento) {
+      throw new TypeError()
+    }
+    let resultado = "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    desplazamiento = (desplazamiento % 26 - 26) % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+
+        if (letras.indexOf(texto[i]) !== -1) {
+          const posicion = ((letras.indexOf(texto[i]) - desplazamiento) % 26);
+          resultado += letras[posicion];
+
+        }
+        else
+          resultado += texto[i];
+      }
+    }
+    return resultado;
+  }
 };
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+//window.addEventListener("load",inicio,true);
+document.getElementById("mensaje").addEventListener("keyup", function () {
+  this.value = this.value.toUpperCase();
+}, true);
+document.getElementById("cifrar").addEventListener("click", function () {
+  const texto = document.getElementById("mensaje").value;
+  const desplazamiento = document.getElementById("desplazamiento").value;
+
+  document.getElementById("mensaje2").value = cipher.encode(desplazamiento, texto);
+
+
+}, true);
+
+document.getElementById("descifrar").addEventListener("click", function () {
+  const texto = document.getElementById("mensaje").value;
+  const desplazamiento = document.getElementById("desplazamiento").value;
+  document.getElementById("mensaje2").value = cipher.decode(desplazamiento, texto);
+
+}, true);
